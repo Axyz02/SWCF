072faa24a8169ccdace0fd139ce88394ba2ee332
Nicolas Ibba Gonzalez <nicolasig002@gmail.com>
2020-11-29 22:00:38 -0600

    Intentando agregar planeta origen [Sin suerte]

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ const name = document.getElementById("name");
 const bYear = document.getElementById("bYear");
 const height = document.getElementById("height");
 const weight = document.getElementById("weight");
-const homeworld = document.getElementById("homeworld");
 const gender = document.getElementById("gender");
 const hColor = document.getElementById("hColor");
-
+const homeworld = document.getElementById("homeworld");
 
 // * Callback function for Event listener
 function getPj() {
@@ -33,11 +32,24 @@ function getPj() {
           alert("That name doesn't match any character")
         } else {
           updateInfo(response.data.results[0]);
+          getHomeworld(response.data.results[0].homeworld);
         }
       });
   }
 
 }
+
+
+// * Get extra data ( another API call )
+function getHomeworld (url){
+  let planetName;
+  axios.get(url)
+  .then(function (response) {
+    planetName = response.data.name;
+    return planetName;
+  })
+};
+
 
 // * Render function
 function updateInfo(data) {
@@ -47,6 +59,7 @@ function updateInfo(data) {
   weight.innerText = `Weight: ${data.mass} Kg.`;
   if (data.gender !== 'n/a') gender.innerText = `Gender: ${data.gender}`; else data.gender = '';
   if (data.hair_color !== 'n/a')hColor.innerText =`Hair color: ${data.hair_color}`; else hColor.innerText = '';
+  homeworld.innerText = `Homeworld: ${getHomeworld(data.homeworld)}`;
 }
 
 
