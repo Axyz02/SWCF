2af2a74599231ac78ae77df19150d3e7b06d3aa2
Axyz02 <nicolasig002@gmail.com>
2020-12-03 16:34:20 -0300

    Agregado planeta | Agregado icono de carga (No funciona)
    TODO: hacer funcionar el planeta de manera que se sincronice con el resto de los datos.
    TODO: Hacer funcionar el icono de carga.

--- main.js
+++ main.js
@@ -13,27 +13,35 @@ const homeworld = document.getElementById("homeworld");
 
 // * Callback function for Event listener
 function getPj() {
-  // * In case of a void search input
-  let randomNum = Math.floor(Math.random() * 88 + 1);
 
+  loadingIcon();
+
+  // * In case of a void search input
+  let randomNum = Math.floor((Math.random() * 88) + 1);
+  const apiUrl = 'https://swapi.dev/api/people/';
   // * Fetching the API with Axios (Ajax)
   if (search.value === '') {
     axios
-      .get("https://swapi.dev/api/people/" + randomNum)
-      .then(function (response) {
+      .get(apiUrl + randomNum)
+      .then(response => {
         updateInfo(response.data);
+        getHomeworld(response.data.homeworld);
+      }).catch(e => {
+        updateInfoWithError();
       });
   }
 
   else {
-    axios.get("https://swapi.dev/api/people/?search=" + search.value)
-      .then(function (response) {
+    axios.get(apiUrl + search.value)
+      .then(response => {
         if (response.data.count === 0) {
           alert("That name doesn't match any character")
         } else {
           updateInfo(response.data.results[0]);
           getHomeworld(response.data.results[0].homeworld);
         }
+      }).catch(e => {
+         updateInfoWithError();
       });
   }
 
@@ -41,25 +49,55 @@ function getPj() {
 
 
 // * Get extra data ( another API call )
-function getHomeworld (url){
+function getHomeworld(url) {
   let planetName;
   axios.get(url)
-  .then(function (response) {
-    planetName = response.data.name;
-    return planetName;
-  })
+    .then(function (response) {
+      // console.log(response.data.name);
+      // planetName = response.data.name;
+      // console.log(planetName);
+      // return planetName;
+      homeworld.innerText = `Homeworld: ${response.data.name}`;
+    })
 };
 
 
-// * Render function
+// * Render functions
 function updateInfo(data) {
   name.innerText = `Name: ${data.name}`;
   bYear.innerText = `Birth year: ${data.birth_year}`;
-   height.innerText = `Height: ${(data.height/100).toFixed(2)} mts.`;
-  weight.innerText = `Weight: ${data.mass} Kg.`;
-  if (data.gender !== 'n/a') gender.innerText = `Gender: ${data.gender}`; else data.gender = '';
-  if (data.hair_color !== 'n/a')hColor.innerText =`Hair color: ${data.hair_color}`; else hColor.innerText = '';
-  homeworld.innerText = `Homeworld: ${getHomeworld(data.homeworld)}`;
+  height.innerText = `Height: ${(data.height / 100).toFixed(2)} mts.`;
+  // ! Datos opcionales
+  if (data.mass !== 'unknown') weight.innerText = `Weight: ${data.mass} Kg.`;
+  else weight.innerText = `Weight: ${data.mass}`;
+
+  if (data.gender !== 'n/a') gender.innerText = `Gender: ${data.gender}`;
+  else data.gender = '';
+
+  if (data.hair_color !== 'n/a') hColor.innerText = `Hair color: ${data.hair_color}`;
+  else hColor.innerText = '';
+
+}
+
+function updateInfoWithError() {
+  name.innerText = `Oh no! That name doesn't match any character.`;
+  bYear.innerText = ``;
+  height.innerText = ``;
+  weight.innerText = ``;
+  gender.innerText = ``;
+  hColor.innerText = ``;
+  homeworld.innerText=``;
+}
+
+
+function loadingIcon() {
+  name.innerHTML = `<i class="fas fa-spinner"></i>`;
+  bYear.innerText = ``;
+  height.innerText = ``;
+  weight.innerText = ``;
+  gender.innerText = ``;
+  hColor.innerText = ``;
+  homeworld.innerText=``;
 }
 
 
